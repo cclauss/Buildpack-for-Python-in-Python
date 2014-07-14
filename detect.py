2844436bdf6440e5a2bd2cbbd99e4761864783b4
cclauss <cclauss@bluewin.ch>
2014-07-14 17:13:13 +0200

    Create detect.py

---
+++ detect.py
@@ -0,0 +1,9 @@
+import os.path, sys
+
+build_dir = sys.argv[1]
+
+if (os.path.isfile(os.path.join(build_dir, 'requirements.txt'))
+ or os.path.isfile(os.path.join(build_dir, 'setup.py'))):
+    print('Python')
+else:
+    sys.exit(1)
